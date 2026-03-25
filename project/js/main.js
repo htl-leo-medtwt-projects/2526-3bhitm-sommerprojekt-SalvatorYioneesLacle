2cb697eb995bbbed5e5c8b197eb216804480a90e
Salvator Yionees Lacle <s.lacle@students.htl-leonding.ac.at>
2026-03-25 08:43:01 +0100

    Add upload page

--- project/js/main.js
+++ project/js/main.js
@@ -7,3 +7,56 @@ function account() {
         });
     }
 }
+
+function initPage() {
+    let str = '';
+
+    str += `
+        <main>
+            <nav>
+                ${initNav()}
+            </nav>
+
+
+            <div class="page-headline">
+                <h2>Create your soundboard</h2>
+            </div>
+        </main>
+    `;
+
+    document.getElementsByTagName('body').item(0).innerHTML = str;
+}
+initPage();
+
+function initNav() {
+    let str = '';
+
+    str += `
+        <div id="nav-btn-box">
+            <div class="nav-left">
+                <a href="" class="nav-btn">
+                    A
+                </a>
+                <a href="" class="nav-btn">
+                    B
+                </a>
+                <a href="" class="nav-btn">
+                    C
+                </a>
+            </div>
+            <div class="nav-account-box">
+                <!-- Generated from figma -->
+                <div class="header-auth" id="headerAuthContainer">
+                    <div class="button-signin">
+                        <div class="button-text">Sign in</div>
+                    </div>
+                    <div class="button-register">
+                        <div class="button-text">Register</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    `;
+
+    return str;
+}
